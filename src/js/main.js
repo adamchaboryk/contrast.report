070b1a3870cd7041290e6f373c10bd6903c0dd4c
Adam Chaboryk <adam.chaboryk@gmail.com>
2025-03-28 00:37:25 -0400

    clean up

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import '../styles/reset.css';
 import '../styles/style.css';
 import '@oddbird/popover-polyfill';
 import { store } from 'sa11y/src/js/utils/utils.js';
-import { normalizeColorInput, transparentHexToSolid } from './utils.js';
+import chroma from 'chroma-js';
+import * as Utils from './utils.js';
 import { updateRatio, updatePreview } from './update.js';
 import pip from './pip.js';
 import initEyeDropper from './eyedropper.js';
@@ -20,28 +21,32 @@ document.querySelector('#app').innerHTML = `
     <h1>Colour Contrast Checker</h1>
     <div id="pip-container">
       <div id="pip-only">
-        <div class="color-inputs">
-
-          <section aria-labelledby="foreground" id="fg-group">
-            <label for="fg-input" id="foreground">Foreground</label>
-            <div class="color-picker-field">
-              <button id="fg-eyedropper" type="button">${eyedropper}</button>
-              <input id="fg-picker" type="color" aria-label="Foreground colour picker" value="${fg}">
-              <input type="text" id="fg-input" value="${fg}" autocomplete="off">
-            </div>
-          </section>
-
-          <section aria-labelledby="background">
-            <label for="bg-input">Background</label>
-            <div class="color-picker-field">
-              <button id="bg-eyedropper" type="button">${eyedropper}</button>
-              <input id="bg-picker" type="color" aria-label="Background colour picker" value="${bg}">
-              <input type="text" id="bg-input" value="${bg}" autocomplete="off">
-            </div>
-          </section>
+        <div class="tri-column">
+          <div class="column-1">
+            <section aria-labelledby="foreground" id="fg-group">
+              <label for="fg-input" id="foreground">Foreground</label>
+              <div class="color-picker-field">
+                <button id="fg-eyedropper" type="button">${eyedropper}</button>
+                <input id="fg-picker" type="color" aria-label="Foreground colour picker" value="${fg}">
+                <input type="text" id="fg-input" value="${fg}" autocomplete="off">
+              </div>
+            </section>
+            <section aria-labelledby="background">
+              <label for="bg-input">Background</label>
+              <div class="color-picker-field">
+                <button id="bg-eyedropper" type="button">${eyedropper}</button>
+                <input id="bg-picker" type="color" aria-label="Background colour picker" value="${bg}">
+                <input type="text" id="bg-input" value="${bg}" autocomplete="off">
+              </div>
+            </section>
+          </div>
+          <div class="column-2">
+            <section aria-labelledby="results">
+              <h2 id="results" class="sr-only">Results</h2>
+              <h2>Ratio <div id="ratio-container"></div> <span id="preview" style="color: ${fg}; background-color: ${bg}">${drop}</span></h2>
+            </section>
+          </div>
         </div>
-
-        <h2>Ratio <div id="ratio-container"></div> <span id="preview" style="color: ${fg}; background-color: ${bg}">${drop}</span></h2>
         <div id="toolbar">
           <button type="button" id="wcag-aa">WCAG AA</button>
           <button type="button" id="reverse">Reverse</button>
@@ -87,38 +92,52 @@ const updateColor = (inputElement, pickerElement, colorValue, colorType) => {
   const inputEl = inputElement;
   const pickerEl = pickerElement;
   inputEl.value = colorValue;
-  pickerEl.value = colorValue;
-  store.setItem(colorType, colorValue);
+  pickerEl.value = chroma(colorValue); // Returns valid 6 digit hex for native color input.
+  store.setItem(colorType, chroma(colorValue)); // Saves validated colour.
   updateRatio();
   updatePreview();
 };
 
 // Register event listeners.
 fgPicker.addEventListener('input', () => {
-  const fgColor = normalizeColorInput(fgPicker.value);
+  const fgColor = Utils.normalizeColorInput(fgPicker.value);
   updateColor(fgInput, fgPicker, fgColor, 'foreground');
 });
 fgInput.addEventListener('input', () => {
-  const fgColor = normalizeColorInput(fgInput.value);
+  const fgColor = Utils.normalizeColorInput(fgInput.value);
   updateColor(fgInput, fgPicker, fgColor, 'foreground');
 });
 bgPicker.addEventListener('input', () => {
-  const bgColor = normalizeColorInput(bgPicker.value);
+  const bgColor = Utils.normalizeColorInput(bgPicker.value);
   updateColor(bgInput, bgPicker, bgColor, 'background');
 });
 bgInput.addEventListener('input', () => {
-  const bgColor = normalizeColorInput(bgInput.value);
+  const bgColor = Utils.normalizeColorInput(bgInput.value);
   updateColor(bgInput, bgPicker, bgColor, 'background');
 });
 
 // Reverse colours button.
 reverseBtn.addEventListener('click', () => {
-  const tempFg = fgInput.value;
-  const tempBg = bgInput.value;
-  fgInput.value = tempBg;
-  bgInput.value = transparentHexToSolid(tempFg);
-  fgPicker.value = tempBg;
-  bgPicker.value = transparentHexToSolid(tempFg);
+  // Retrieve current foreground and background values from localStorage.
+  const tempFg = store.getItem('foreground');
+  const tempBg = store.getItem('background');
+
+  // Reverse the color values.
+  // Remove alpha for background.
+  const reversedFg = tempBg;
+  const reversedBg = Utils.transparentHexToSolid(tempFg);
+
+  // Update both input fields and color pickers.
+  fgInput.value = reversedFg;
+  bgInput.value = reversedBg;
+  fgPicker.value = reversedFg;
+  bgPicker.value = reversedBg;
+
+  // Update the localStorage values.
+  store.setItem('foreground', reversedFg);
+  store.setItem('background', reversedBg);
+
+  // Update ratio and preview with the reversed colors.
   updateRatio();
   updatePreview();
 });
--- src/js/utils.js
+++ src/js/utils.js
@@ -22,10 +22,20 @@ export function normalizeColorInput(color) {
   if (maybeHex && !reservedWords.has(normalizedColor.toLowerCase())) {
     normalizedColor = `#${normalizedColor}`;
   }
+
   return normalizedColor;
 }
 
 export function transparentHexToSolid(hex) {
   if (hex.length === 9) return hex.slice(0, 7);
+  return hex;
+}
+
+export function convertShortHex(hex) {
+  // Check if the hex code is 3 characters long and starts with a '#'
+  if (hex.length === 4 && hex[0] === '#') {
+    // Convert it to 6 characters by repeating each character
+    return `#${hex[1].repeat(2)}${hex[2].repeat(2)}${hex[3].repeat(2)}`;
+  }
   return hex;
 }
